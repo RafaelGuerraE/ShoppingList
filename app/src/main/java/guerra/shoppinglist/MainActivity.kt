8d1f45de67664526935c377de2376d771d15d5e5
Rafael Guerra <rafaelge.19@outlook.com.br>
2024-12-27 16:53:09 -0300

    Initial Commit

---
+++ app/src/main/java/guerra/shoppinglist/MainActivity.kt
@@ -0,0 +1,40 @@
+package guerra.shoppinglist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import guerra.shoppinglist.ui.theme.ShoppingListTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShoppingListTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ShoppingList(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+
+}
